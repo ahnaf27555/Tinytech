4d252c1cccdbb11f86cee40a05c4be87a90770c8
Ahnaf Fahmid Iqbal <ahnaf27555@gmail.com>
2025-09-05 17:00:35 +0600

    fixing filter alignment attempt-1

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Filters({ setCategory }) {
   return (
-    <div className="d-flex justify-content-center mb-4">
+    <div className="d-flex flex-wrap justify-content-center mb-4">
       <button className="btn btn-outline-dark mx-2" onClick={() => setCategory("All")}>All</button>
       <button className="btn btn-outline-dark mx-2" onClick={() => setCategory("animeAccessories")}>Anime Accessories</button>
       <button className="btn btn-outline-dark mx-2" onClick={() => setCategory("modelCar")}>Model Car</button>
